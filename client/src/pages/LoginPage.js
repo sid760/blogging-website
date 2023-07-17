cfd39a85177a4e9588abb19c3b239add51873d33
Atharv <adasare18@gmail.com>
2023-07-17 09:54:18 +0530

    bg animation

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,15 +1,28 @@
-import {useContext, useState} from "react";
+import {useContext, useState,useRef,useEffect} from "react";
 import {Navigate} from "react-router-dom";
 import {UserContext} from "../UserContext";
 import React from "react";
-import Lottie from "lottie-react";
+import lottie from "lottie-web";
+import animationData from '../assets/login_bg.json';
+
 // import groovyWalkAnimation from "./groovyWalk.json";
 
 export default function LoginPage() {
+  const animate=useRef(null);
   const [username,setUsername] = useState('');
   const [password,setPassword] = useState('');
   const [redirect,setRedirect] = useState(false);
   const {setUserInfo} = useContext(UserContext);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animate.current,
+      animationData: animationData,
+      renderer: 'svg', // or 'canvas', 'html', 'svg'
+      loop: true, // set to true or false
+      autoplay: true, // set to true or false
+    });
+  }, []);
   async function login(ev) {
     ev.preventDefault();
     const response = await fetch('http://localhost:4000/login', {
@@ -32,6 +45,8 @@ export default function LoginPage() {
     return <Navigate to={'/'} />
   }
   return (
+    <div>
+    <div ref={animate} className="anime"></div>
     <form className="login" onSubmit={login}>
       <h1>Login</h1>
       <input type="text"
@@ -44,5 +59,6 @@ export default function LoginPage() {
              onChange={ev => setPassword(ev.target.value)}/>
       <button>Login</button>
     </form>
+    </div>
   );
 }
